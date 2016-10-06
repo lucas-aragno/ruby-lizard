7f5e2cc1c1393323f82de5a4b8eb156e301890a7
lucas <lucas.aragno157@gmail.com>
2016-10-06 17:13:51 -0300

    Serving public stuff now

--- app.rb
+++ app.rb
@@ -4,7 +4,3 @@ require_relative "config/init"
 get "/" do
   send_file File.join(settings.public_folder, 'index.html')
 end
-
-get "/javascript/:file" do
-  send_file File.join("#{settings.public_folder}/js/#{params[:file]}")
-end
